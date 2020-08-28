f4f8fd64e3dadbe949123d3721e0cfceda49b8f9
R.Ito <kawboy442@gmail.com>
2020-08-29 03:04:57 +0900

    初回コミット

---
+++ chapl-3-1/chapl-3-1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  chapl-3-1
+//
+//  Created by 伊藤僚亮 on 2020/08/29.
+//  Copyright © 2020 伊藤僚亮. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
